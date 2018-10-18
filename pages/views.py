3a66ca0ef52f5708a49b75d4d047a0048899bb74
balumn001 <balumnandan@gmail.com>
2018-10-18 11:16:49 +0530

    Thumbnail for Files added

--- pages/views.py
+++ pages/views.py
@@ -69,7 +69,7 @@ def homme(request):
     for x in list_files(drive_service):
         if x.get('title') and x.get('alternateLink'):
             fid="https://drive.google.com/uc?export=download&id="+x['id']
-            xx.append({'title':x['title'],'altLink':x['alternateLink'],'dL':fid})
+            xx.append({'title':x['title'],'altLink':x['alternateLink'],'dL':fid,'iLink':x['iconLink']})
     with open("data_file.json", "w") as fList: 
         json.dump(xx, fList)
     yy=[]
